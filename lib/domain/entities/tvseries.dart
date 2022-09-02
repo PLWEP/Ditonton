3e442b9d29b7d5d6e610701e831d55d1dbffd80e
PLWEP <langgeng86@gmail.com>
2022-09-02 21:30:35 +0700

    create tv series entity and repository

---
+++ lib/domain/entities/tvseries.dart
@@ -0,0 +1,48 @@
+import 'package:equatable/equatable.dart';
+
+class Tvseries extends Equatable {
+  Tvseries({
+    required this.backdropPath,
+    required this.genresId,
+    required this.id,
+    required this.originalName,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.status,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  String? backdropPath;
+  List<int>? genresId;
+  int? id;
+  String? originalName;
+  String? overview;
+  double? popularity;
+  String? posterPath;
+  String? status;
+  double? voteAverage;
+  int? voteCount;
+
+  Tvseries.watchlist({
+    required this.id,
+    required this.overview,
+    required this.posterPath,
+    required this.originalName,
+  });
+
+  @override
+  List<Object?> get props => [
+        backdropPath,
+        genresId,
+        id,
+        originalName,
+        overview,
+        popularity,
+        posterPath,
+        status,
+        voteAverage,
+        voteCount
+      ];
+}
